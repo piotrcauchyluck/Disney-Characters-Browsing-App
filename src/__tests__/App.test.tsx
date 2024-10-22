eefcf3b12648f5d55378f174f69ad2048664a45c
PK <koszelak.piotr@gmail.com>
2024-10-22 23:17:07 +0200

    Update initial ut

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,13 +1,14 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { http, HttpResponse } from 'msw';
 import { setupServer } from 'msw/node';
+
+import { dataPageSizeThree } from './dummyData';
+
 import App from '../App';
 
 const server = setupServer(
-    http.post(`${import.meta.env.VITE_SERVER_URL}/chat`, () => {
-        return HttpResponse.json({
-            reply: 'hello there',
-        });
+    http.get(`https://api.disneyapi.dev/character?pageSize=3`, () => {
+        return HttpResponse.json(dataPageSizeThree);
     })
 );
 
@@ -15,48 +16,7 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test('Displays all elements', async () => {
+test('Displays all elements', () => {
     render(<App />);
-
-    expect(screen.getByText('Chat Bot')).toBeInTheDocument();
-    expect(screen.getByTestId('input')).toBeInTheDocument();
-    expect(screen.getByRole('button')).toBeInTheDocument();
-});
-
-test('Displays a new answer', async () => {
-    render(<App />);
-
-    expect(screen.queryByRole('answer')).not.toBeInTheDocument();
-
-    const input = screen.getByTestId('input');
-    fireEvent.change(input, { target: { value: 'Hello' } });
-
-    fireEvent.click(screen.getByText('Send'));
-
-    await screen.findByRole('answer');
-
-    expect(screen.getByRole('answer')).toHaveTextContent('hello there');
-});
-
-test('Displays proper error', async () => {
-    server.use(
-        http.post(`${import.meta.env.VITE_SERVER_URL}/chat`, () => {
-            return new HttpResponse(null, { status: 500 });
-        })
-    );
-
-    render(<App />);
-
-    expect(screen.queryByRole('answer')).not.toBeInTheDocument();
-
-    const input = screen.getByTestId('input');
-    fireEvent.change(input, { target: { value: 'Hello' } });
-
-    fireEvent.click(screen.getByText('Send'));
-
-    await screen.findByRole('answer');
-
-    expect(screen.getByRole('answer')).toHaveTextContent(
-        'Oops, failed to fetch!'
-    );
+    expect(screen.getByText('Home')).toBeInTheDocument();
 });
