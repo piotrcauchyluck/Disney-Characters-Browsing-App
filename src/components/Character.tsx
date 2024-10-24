b871bfb0584e8c89532c08839b85dcf68ed723ec
PK <koszelak.piotr@gmail.com>
2024-10-24 21:13:02 +0200

    Refine details view layout

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,36 +1,57 @@
-import styled from 'styled-components';
-
 import type { Character } from '../types/data';
 import { mappedNames } from '../utils/mappedData';
+import { mobileThreshold, size } from '../utils/constants';
 
+import { Box, colors, styled, Typography } from '@mui/material';
 import Image from './Image';
-import { Typography } from '@mui/material';
+import { useIsMobile } from '../hooks/common';
 
-const StyledContainer = styled.div`
-    width: 100%;
+const StyledContainer = styled(Box)`
+    width: min(90%, ${size.defaultListWidth}px);
     display: flex;
-`;
-
-const StyledImageContainer = styled.div`
-    width: 30%;
-    border-right: 2px solid black;
-    box-sizing: border-box;
-`;
-
-const StyledDescriptionContainer = styled.div`
-    padding: 20px;
-    width: 70%;
+    margin: 0 auto;
+    padding-top: 20px;
+    gap: 30px;
+    justify-content: center;
 `;
 
 const StyledName = styled(Typography)`
-    margin-bottom: 20px;
-    color: orange;
+    color: ${colors.orange[700]};
+`;
+
+const StyledDescription = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding-left: 30px;
+    border-left: 2px solid ${colors.grey[300]};
+    @media (max-width: ${mobileThreshold}px) {
+        border-left: none;
+        padding-left: 0;
+    }
+
+    .main-list {
+        padding-inline-start: 0;
+        list-style-type: none;
+        @media (max-width: ${mobileThreshold}px) {
+            font-size: 12px;
+        }
+    }
+`;
+
+const StyledImage = styled(Image)`
+    border-radius: 10px;
+`;
+
+const StyledMobileImage = styled(StyledImage)`
+    display: block;
+    margin: 0 auto;
 `;
 
 type CharacterProps = { data: Character };
 
 const CharacterDescription = ({ data }: CharacterProps) => (
-    <ul>
+    <ul className="main-list">
         {Object.entries(data).map(([info, InfoValue]) => {
             if (!Array.isArray(InfoValue)) return;
 
@@ -52,17 +73,20 @@ const Character = (props: CharacterProps) => {
     const { data } = props;
     const { name, imageUrl } = data;
 
+    const isMobile = useIsMobile();
+
     return (
         <StyledContainer>
-            <StyledImageContainer>
-                <Image src={imageUrl} alt={name} />
-            </StyledImageContainer>
-            <StyledDescriptionContainer>
-                <StyledName variant="h4" color="textSecondary">
-                    {name}
-                </StyledName>
+            {!isMobile && (
+                <Box>
+                    <StyledImage src={imageUrl} alt={name} />
+                </Box>
+            )}
+            <StyledDescription>
+                <StyledName variant="h5">{name}</StyledName>
+                {isMobile && <StyledMobileImage src={imageUrl} alt={name} />}
                 <CharacterDescription {...props} />
-            </StyledDescriptionContainer>
+            </StyledDescription>
         </StyledContainer>
     );
 };
