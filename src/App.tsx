b95f29c9bcc027a54caa1b8029541f82986dd3ec
PK <koszelak.piotr@gmail.com>
2024-10-24 21:21:41 +0200

    Disable refetch

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,13 @@ import NoMatch from './views/noMatch/NoMatch';
 
 import './App.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        },
+    },
+});
 
 const App = () => {
     return (
