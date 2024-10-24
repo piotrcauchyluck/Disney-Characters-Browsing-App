2d6e237dceae47e5402340162ea046a395bc4014
PK <koszelak.piotr@gmail.com>
2024-10-24 16:31:22 +0200

    Refactor image component

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,23 +1,9 @@
-import styled from 'styled-components';
-import type { Character } from '../types/data';
+import { Box } from '@mui/material';
 
-type ImageProps = Pick<Character, 'imageUrl' | 'name'>;
-type StyledImageProps = Pick<ImageProps, 'imageUrl'> & { alt: string };
-
-const StyledImage = styled.image<StyledImageProps>`
-    background-image: url(${({ imageUrl }) => imageUrl});
-    display: block;
-    width: 100%;
-    height: 100%;
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center;
-`;
+type ImageProps = Record<string, unknown>;
 
 const Image = (props: ImageProps) => {
-    const { imageUrl, name } = props;
-
-    return <StyledImage imageUrl={imageUrl} alt={name} />;
+    return <Box component="img" {...props} />;
 };
 
 export default Image;
