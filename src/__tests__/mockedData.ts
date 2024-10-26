3b89b8b2c0d21538f6f160c7e413124006f8ac22
PK <koszelak.piotr@gmail.com>
2024-10-26 13:15:23 +0200

    Update mocked data and add test utils

---
+++ src/__tests__/mockedData.ts
@@ -0,0 +1,101 @@
+export const dataJackSparrow = {
+    info: {
+        count: 2,
+        totalPages: 1,
+        previousPage: null,
+        nextPage: null,
+    },
+    data: [
+        {
+            _id: 3322,
+            films: [
+                "Pirates of the Caribbean: At World's End",
+                'Pirates of the Caribbean: On Stranger Tides',
+            ],
+            shortFilms: [],
+            tvShows: [],
+            videoGames: [],
+            parkAttractions: [
+                'The Legend of Captain Jack Sparrow',
+                "Trader Sam's",
+            ],
+            allies: [],
+            enemies: [],
+            sourceUrl: 'https://disney.fandom.com/wiki/Jack_Sparrow%27s_Mother',
+            name: "Jack Sparrow's Mother",
+            imageUrl:
+                'https://static.wikia.nocookie.net/disney/images/f/f6/Jacks_Mum.PNG.png',
+            createdAt: '2021-04-12T02:14:22.544Z',
+            updatedAt: '2021-12-20T20:39:50.608Z',
+            url: 'https://api.disneyapi.dev/characters/3322',
+            __v: 0,
+        },
+        {
+            _id: 6322,
+            films: [
+                'Pirates of the Caribbean: The Curse of the Black Pearl',
+                "Pirates of the Caribbean: Dead Man's Chest",
+                "Pirates of the Caribbean: At World's End",
+                'Pirates of the Caribbean: On Stranger Tides',
+                'Pirates of the Caribbean: Dead Men Tell No Tales',
+            ],
+            shortFilms: [],
+            tvShows: [],
+            videoGames: [
+                'Pirates of the Caribbean: The Curse of the Black Pearl (video game)',
+                'Kingdom Hearts (series)',
+                'Pirates of the Caribbean: The Legend of Jack Sparrow',
+                "Pirates of the Caribbean: Dead Man's Chest (video game)",
+                "Pirates of the Caribbean: At World's End (video game)",
+                'Lego Pirates of the Caribbean: The Video Game',
+                'Disney Universe',
+                'Disney Magical World',
+                'Disney Infinity (series)',
+                'Disney Magic Kingdoms',
+                'Disney Crossy Road',
+                'Disney Emoji Blitz',
+                'Disney Heroes: Battle Mode',
+                'Disney Epic Quest',
+                'Disney Tsum Tsum (game)',
+                "Disney Sorcerer's Arena",
+                'Disney Getaway Blast',
+                'Disney Mirrorverse',
+            ],
+            parkAttractions: [
+                'Pirates of the Caribbean (ride)',
+                'World of Color',
+                "Mickey's Boo-to-You Halloween Parade",
+                'The Legend of Captain Jack Sparrow',
+                "A Pirate's Adventure: Treasures of the Seven Seas",
+                'Pirates of the Caribbean: Battle for the Sunken Treasure',
+                "Eye of the Storm: Captain Jack's Stunt Spectacular",
+                'Ignite the Dream: A Nighttime Spectacular of Magic and Light',
+                'The Nightmare Experiment',
+                'Pirates Summer Battle "Get Wet!"',
+                'Fantasmic!',
+            ],
+            allies: [],
+            enemies: [],
+            sourceUrl: 'https://disney.fandom.com/wiki/Jack_Sparrow',
+            name: 'Jack Sparrow',
+            imageUrl:
+                'https://static.wikia.nocookie.net/disney/images/2/28/Profile_-_Jack_Sparrow.png',
+            createdAt: '2021-04-12T02:55:28.469Z',
+            updatedAt: '2021-12-20T20:40:20.699Z',
+            url: 'https://api.disneyapi.dev/characters/6322',
+            __v: 0,
+        },
+    ],
+};
+
+export const sampleCharacter = dataJackSparrow.data[0];
+
+export const noData = {
+    info: {
+        count: 0,
+        totalPages: 0,
+        previousPage: null,
+        nextPage: null,
+    },
+    data: [],
+};
---
+++ src/__tests__/test-utils.tsx
@@ -0,0 +1,32 @@
+import React, { PropsWithChildren } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import type { RenderOptions } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+export function renderWithRouter(
+    ui: React.ReactElement,
+    renderOptions: RenderOptions = {}
+) {
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: false,
+            },
+        },
+    });
+
+    const Wrapper = ({ children }: PropsWithChildren) => (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={children} />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
+    );
+
+    return {
+        ...render(ui, { wrapper: Wrapper, ...renderOptions }),
+    };
+}
